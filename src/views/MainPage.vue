b6b588f07b862132715250bac52e287b1afd105c
esmanurbtmz <esmabitmez@hotmail.com>
2023-04-11 22:50:42 +0300

    .

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,9 +1,25 @@
 <script setup>
 import appHeader from "../components/appHeader.vue";
+import cityComponent from "./cityComponent.vue"
 </script>
 
 <template>
     <div>
         <app-header></app-header>
+        <div class="main container-fluid mt-5">
+            <div class="row d-flex justify-content-end">
+                <div class="col-lg-5">deneme</div>
+            <div class="col-lg-5"></div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+.main{
+    overflow: hidden !important;
+    height: 70vh;
+    background-image: url("../../medias/images/spring-background-2020.png");
+    background-repeat: no-repeat;
+}
+</style>
