0faf8ad62518db4e96142e92c1c8a55f90ffeeb3
esmanurbtmz <esmabitmez@hotmail.com>
2023-04-15 21:37:18 +0300

    login page design

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,4 +1,84 @@
 <script></script>
 <template>
-    <h1>login</h1>
+  <div class="d-flex justify-content-center align-items-center f-container">
+    
+    <div class="col-lg-3 f-box">
+        <router-link class="close-btn" :to="{name:'MainPage'}"><i class="fa fa-times d-flex justify-content-end" aria-hidden="true"></i></router-link>
+        <h2 class="text-center">Üye Girişi</h2>
+        <button class="btn facebook-btn mt-3"><b>Facebook ile Giriş Yap</b></button>
+        <button class="btn google-btn mt-3">Google il Giriş Yap</button>
+        <hr>
+      <div class="form-floating mb-3">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+        />
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating mb-2">
+        <input
+          type="password"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+        />
+        <label for="floatingPassword mb-">Password</label>
+      </div>
+      <div>
+        <a href=""><b>Şifrenizi mi Unnutunuz?</b></a>
+      </div>
+      <button class="login-btn btn mt-3"><b>Giriş Yap</b></button>
+      <p class="text-center mt-2"><b>Henüz üye değil misiniz</b></p>
+      <button class="btn register-btn"><b>Üye Ol</b></button>
+    </div>
+  </div>
 </template>
+<style scoped>
+.f-container{
+    height: 100vh;
+}
+.form-floating  input{
+    border-bottom: 1px solid rgb(158, 158, 158) !important ;
+    border-radius: 0px;
+}
+.f-box{
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px 5px rgb(213, 213, 213);
+    padding: 50px;
+}
+.facebook-btn{
+    width: 100%;
+    background-color: #45619d;
+    color: white;
+}
+.google-btn{
+    width: 100%;
+    background-color: #4285f4;
+    color: white;
+}
+a{
+    color: rgb(78, 78, 78);
+    font-size: 13px;
+    display: flex;
+    justify-content: end;
+}
+.login-btn{
+    background-color: #d23b38;
+    width: 100%;
+    color: white;
+}
+p{
+    font-size: 13px;
+}
+.register-btn{
+    color: #d23b38;
+    width: 100%;
+    border: 1px solid #d23b38 ;
+}
+.close-btn{
+    text-decoration: none;
+    font-size: 20px;
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,19 +1,26 @@
-<script setup>
+<script>
 import appHeader from "../components/appHeader.vue";
 import cityComponent from "./cityComponent.vue";
+export default {
+  components:{appHeader,cityComponent},
+};
 </script>
 
 <template>
   <div>
     <app-header></app-header>
     <div class="main container-fluid mt-5">
-      <div class="row flex-wrap d-flex justify-content-center align-items-center">
+      <div
+        class="row flex-wrap d-flex justify-content-center align-items-center"
+      >
         <div class="col-lg-4">
           <city-component></city-component>
         </div>
-        <div class=" col-lg-6 b-container">
-          <h3 class="text-center pt-4">Türkiye'nin En Büyük Otobüs Bileti Sitesi</h3>
-          <div class="row  justify-content-around">
+        <div class="col-lg-6 b-container">
+          <h3 class="text-center pt-4">
+            Türkiye'nin En Büyük Otobüs Bileti Sitesi
+          </h3>
+          <div class="row justify-content-around">
             <div class="box col-lg-3 me-1">
               <i class="fa fa-headphones" aria-hidden="true"></i>
               <div>7/24 Müşteri Hizmeti</div>
@@ -22,7 +29,7 @@ import cityComponent from "./cityComponent.vue";
               <i class="fa fa-credit-card" aria-hidden="true"></i>
               <div>Güvenli Ödeme</div>
             </div>
-            <div class="box col-lg-3 ">
+            <div class="box col-lg-3">
               <i class="fa fa-database" aria-hidden="true"></i>
               <div>Komisyon Yok, Ücretsiz</div>
             </div>
@@ -32,11 +39,17 @@ import cityComponent from "./cityComponent.vue";
             </div>
             <div class="box col-lg-3">
               <i class="fa fa-bell" aria-hidden="true"></i>
-              <div>İki Dakikada <br> Biletini Al</div>
+              <div>
+                İki Dakikada <br />
+                Biletini Al
+              </div>
             </div>
             <div class="box col-lg-3">
               <i class="fa fa-undo" aria-hidden="true"></i>
-              <div>İptal Edilen Bilete <br> Kesintisiz İade</div>
+              <div>
+                İptal Edilen Bilete <br />
+                Kesintisiz İade
+              </div>
             </div>
           </div>
         </div>
@@ -45,21 +58,27 @@ import cityComponent from "./cityComponent.vue";
     <h3 class="text-center">Otobüs Bileti Kampanyaları</h3>
     <div class="d-flex justify-content-evenly">
       <div class="col-lg-3 col-6 t-company text-center">
-        <img src="../../medias/images/146-md.png" alt="">
-        <h4 class=" pt-3">CardFinans'tan ParaPuan ve Taksit Kampanyası</h4>
-        <a href="">CardFinans bireysel kredi kartlarınızla yapacağınız 500 TL ve üzeri alışverişinize 5...</a>
+        <img src="../../medias/images/146-md.png" alt="" />
+        <h4 class="pt-3">CardFinans'tan ParaPuan ve Taksit Kampanyası</h4>
+        <a href=""
+          >CardFinans bireysel kredi kartlarınızla yapacağınız 500 TL ve üzeri
+          alışverişinize 5...</a
+        >
       </div>
       <div class="col-lg-3 col-6 t-company">
-        <img src="../../medias/images/148-md.png" alt="">
-        <h4 class=" pt-3">HSBC Puan ve Taksit Kampanyası</h4>
-        <a href="">HSBC bireysel kredi kartlarınızla yapacağınız 500 TL ve üzeri alışverişinize 50 TL, ...</a>
+        <img src="../../medias/images/148-md.png" alt="" />
+        <h4 class="pt-3">HSBC Puan ve Taksit Kampanyası</h4>
+        <a href=""
+          >HSBC bireysel kredi kartlarınızla yapacağınız 500 TL ve üzeri
+          alışverişinize 50 TL, ...</a
+        >
       </div>
     </div>
   </div>
 </template>
 
 <style>
-.main { 
+.main {
   height: 70vh;
   background-image: url("../../medias/images/spring-background-2020.png");
   background-repeat: no-repeat;
@@ -69,7 +88,7 @@ import cityComponent from "./cityComponent.vue";
 }
 .box {
   margin-top: 20px;
-  padding:30px 0px 30px 0px ;
+  padding: 30px 0px 30px 0px;
   color: white;
   background-color: black;
   opacity: 0.8;
@@ -77,7 +96,7 @@ import cityComponent from "./cityComponent.vue";
   text-align: center;
   border-radius: 10px;
 }
-.box i{
+.box i {
   padding-bottom: 5px;
   font-size: 60px;
   color: white;
@@ -86,28 +105,27 @@ import cityComponent from "./cityComponent.vue";
   .b-container {
     display: none;
   }
-  .main{
+  .main {
     background-image: none;
   }
 }
-h3{
+h3 {
   color: rgb(91, 91, 91);
 }
-.t-company img{
+.t-company img {
   width: 100%;
   border-radius: 10px;
 }
-h4{
+h4 {
   color: rgb(91, 91, 91);
 }
-.t-company{
-  box-shadow: 2px 2px 2px rgb(174, 174, 174) ;
+.t-company {
+  box-shadow: 2px 2px 2px rgb(174, 174, 174);
 }
-.t-company a{
+.t-company a {
   text-decoration: none;
   color: rgb(87, 87, 87);
   font-size: 18px;
   padding-bottom: 10px;
-  
 }
 </style>
