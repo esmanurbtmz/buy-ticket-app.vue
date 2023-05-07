1c144c68e3638600e8355e902485d645fa0c1b1e
esmanurbtmz <esmabitmez@hotmail.com>
2023-05-07 17:12:05 +0300

    login has been done

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
-import {createStore} from "vuex"
+import { createStore } from "vuex";
+import createPersistedSatet from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+
+var ls = new SecureLS({ encodingType: "HmacSHA1" });
 
 export default createStore({
-    state: {
-        user: null,
-        saltKey: "register123!456?"
+  state: {
+    user: null,
+    saltKey: "register123!456?",
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
     },
-    getters: {
-        _isAuthenticated: state => state.user !== null,
-        _getCurrentUser(state){
-            const user = state.user
-            delete user?.password
-            return user
-        },
-        _saltKey: state => state.saltKey
-    }
-})
+  },
+  getters: {
+    _isAuthenticated: (state) => state.user !== null,
+    _getCurrentUser(state) {
+      const user = state.user;
+      delete user?.password;
+      return user;
+    },
+    _saltKey: (state) => state.saltKey,
+  },
+  plugins: [
+    createPersistedSatet({
+      key: "user",
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
+});
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -16,7 +16,13 @@ export default{
           this.$store.getters._saltKey
         ).toString();
         this.$appAxios.get(`/users?username=${this.userData.email}&password=${password}`).then(login_res => {
-          console.log(login_res)
+          if(login_res?.data?.length >0){
+            this.$store.commit("setUser",login_res?.data[0])
+            this.$router.push({name:"MainPage"})
+          }
+          else{
+            alert("Böyle bir kullanıcı bulunamadı")
+          }
         })
         .catch(e=> console.log(e))
     }
